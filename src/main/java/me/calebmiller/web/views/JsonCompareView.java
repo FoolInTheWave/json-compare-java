57c25f41fa15db7129fd4a12d388bb5be75345e6
Caleb Miller <cmiller109@oreillyauto.com>
2021-04-25 15:12:13 -0400

    updated vaadin version from 17 to 19, cleaned up some unused code, moved styles from js to css files

--- src/main/java/me/calebmiller/web/views/JsonCompareView.java
+++ src/main/java/me/calebmiller/web/views/JsonCompareView.java
@@ -7,31 +7,32 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.oreillyauto.testutil.view.model.FieldComparison;
 import com.vaadin.flow.component.ClientCallable;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
 import me.calebmiller.web.comparator.JsonObjectComparator;
-import me.calebmiller.web.views.main.MainView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-// TODO convert to endpoint
-@Route(value = "compare", layout = MainView.class)
-@PageTitle("Hello World")
-@CssImport("./styles/views/helloworld/hello-world-view.css")
-public class CompareView extends HorizontalLayout {
+@Route(value = "compare")
+@RouteAlias(value = "")
+@PageTitle("JSON Compare")
+@CssImport("./themes/json-compare-java/views/json-compare-view.css")
+@CssImport(value = "./themes/json-compare-java/components/vaadin-grid.css", themeFor = "vaadin-grid")
+public class JsonCompareView extends HorizontalLayout {
 
-	private final Logger logger = LoggerFactory.getLogger(CompareView.class);
-
-	@Id("errorDialog")
-	private ErrorDialog errorDialog;
+	private final Logger logger = LoggerFactory.getLogger(JsonCompareView.class);
 
 	private static final ObjectMapper objectMapper = new ObjectMapper();
 	private static final transient JsonObjectComparator jsonObjectComparator = new JsonObjectComparator();
@@ -43,13 +44,17 @@ public class CompareView extends HorizontalLayout {
 	private TextArea uiTextArea2;
 	private Button uiCompareButtonn;
 
-	public CompareView() {
-		setId("compare-view");
+	public JsonCompareView() {
+		setId("json-compare-view");
+		addClassName("hello-world-view");
+
 		uiTextArea1 = new TextArea("JSON Object 1");
 		uiTextArea2 = new TextArea("JSON Object 2");
 		uiCompareButtonn = new Button("Compare");
 		add(uiTextArea1, uiTextArea2, uiCompareButtonn);
+
 		setVerticalComponentAlignment(Alignment.END, uiTextArea1, uiCompareButtonn);
+
 		uiCompareButtonn.addClickListener( e-> {
 			Notification.show("Compare button pressed");
 		});
@@ -67,7 +72,7 @@ public class CompareView extends HorizontalLayout {
 //				getElement().callJsFunction("_initializeGrid");
 			} catch (JsonProcessingException e) {
 				logger.error("error parsing comparison result into JSON: {}", e.getMessage());
-				errorDialog.open("Error parsing comparison result into JSON.");
+				showErrorDialog("Error parsing comparison result into JSON.");
 			}
 		}
 	}
@@ -83,7 +88,20 @@ public class CompareView extends HorizontalLayout {
 			object2 = jsonNode2.isObject() ? (ObjectNode) jsonNode2 : JsonNodeFactory.instance.objectNode();
 		} catch (JsonProcessingException e) {
 			logger.error("error parsing json into object: {}", e.getMessage());
-			errorDialog.open("Error parsing entered JSON into object.");
+			showErrorDialog("Error parsing entered JSON into object.");
 		}
+	}
+
+	private void showErrorDialog(String message) {
+		Dialog dialog = new Dialog();
+		dialog.add(new Text(message));
+		dialog.setCloseOnEsc(false);
+		dialog.setCloseOnOutsideClick(false);
+
+		Button confirmButton = new Button("OK", event -> {
+			dialog.close();
+		});
+
+		dialog.add(new Div(confirmButton));
 	}
 }
